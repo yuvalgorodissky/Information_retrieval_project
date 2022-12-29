a647ced3365f7614b234d520b2d37627eb1764a5
yuvalgorodissky <104304481+yuvalgorodissky@users.noreply.github.com>
2022-12-29 11:59:28 +0200

    final version 1.0 all functions complete add functions.py ,inveted_index_makers.ipynb, inveted_index_gcp.py

---
+++ functions.py
@@ -0,0 +1,132 @@
+import csv
+from collections import defaultdict, Counter
+import numpy as np
+from flask import Flask, request, jsonify
+
+import pandas as pd
+import math
+from pathlib import Path
+
+
+def cosine_similarity(query_to_search, index):
+    """
+    Calculate the cosine similarity for each candidate document in D and a given query (e.g., Q).
+    Generate a dictionary of cosine similarity scores
+    key: doc_id
+    value: cosine similarity score
+
+    Parameters:
+    -----------
+    D: DataFrame of tfidf scores.
+
+    Q: vectorized query with tfidf scores
+
+    Returns:
+    -----------
+    dictionary of cosine similarity score as follows:
+                                                                key: document id (e.g., doc_id)
+                                                                value: cosine similarty score.
+    """
+    # YOUR CODE HERE
+    sim_dict = defaultdict(float)
+    query_dict = Counter(query_to_search)
+    N = len(index.DL)
+    for w, pls in index.posting_lists_iter_by_query(query_to_search):
+        wdf = index.df[w]
+        wqtf = query_dict[w]
+        for doc_id, freq in pls:
+            sim_dict[doc_id] += (freq / index.DL[doc_id]) * math.log(N / wdf, 10) * wqtf * index.nf[doc_id]
+    QL = len(query_to_search)
+    for doc_id in sim_dict.keys():
+        sim_dict[doc_id] = sim_dict[doc_id] / QL
+
+    return sim_dict
+
+
+def boolean_similarity(query_to_search, index):
+    sim_dict = defaultdict(float)
+    for w, pls in index.posting_lists_iter_by_query(query_to_search):
+        for doc_id, freq in pls:
+            sim_dict[doc_id] += 1
+    return sim_dict
+
+
+def merge_results(title_scores, body_scores, title_weight=0.5, text_weight=0.5, N=3):
+    """
+    This function merge and sort documents retrieved by its weighte score (e.g., title and body).
+
+    Parameters:
+    -----------
+    title_scores: a dictionary build upon the title index of queries and tuples representing scores as follows:
+                                                                            key: query_id
+                                                                            value: list of pairs in the following format:(doc_id,score)
+
+    body_scores: a dictionary build upon the body/text index of queries and tuples representing scores as follows:
+                                                                            key: query_id
+                                                                            value: list of pairs in the following format:(doc_id,score)
+    title_weight: float, for weigted average utilizing title and body scores
+    text_weight: float, for weigted average utilizing title and body scores
+    N: Integer. How many document to retrieve. This argument is passed to topN function. By default N = 3, for the topN function.
+
+    Returns:
+    -----------
+    dictionary of querires and topN pairs as follows:
+                                                        key: query_id
+                                                        value: list of pairs in the following format:(doc_id,score).
+    """
+    # YOUR CODE HERE
+    merge_dict = defaultdict(float)
+    for d, weight in [(title_scores, title_weight), (body_scores, text_weight)]:
+        for doc_id, score in d.items():
+            merge_dict[doc_id] += score * weight
+    return merge_dict
+
+
+def BM25(query_to_search, index, AVGDL, b=0.75, k1=1.5, k3=1.5, base_log=10):
+    sim_dict = defaultdict(float)
+    query_dict = Counter(query_to_search)
+    N = len(index.DL)
+    for w, pls in index.posting_lists_iter_by_query(query_to_search):
+        wdf = index.df[w]
+        wqtf = query_dict[w]
+        for doc_id, freq in pls:
+            numerator = (k1 + 1) * freq * math.log((N + 1) / wdf, base_log) * (k3 + 1) * wqtf
+            denominator = freq + k1 * (1 - b + b * index.DL[doc_id] / AVGDL) * (k3 + wqtf)
+            sim_dict[doc_id] += (numerator / denominator)
+
+
+def get_top_n(sim_dict, N=100):
+    """
+    Sort and return the highest N documents according to the cosine similarity score.
+    Generate a dictionary of cosine similarity scores
+
+    Parameters:
+    -----------
+    sim_dict: a dictionary of similarity score as follows:
+                                                                key: document id (e.g., doc_id)
+                                                                value: similarity score. We keep up to 5 digits after the decimal point. (e.g., round(score,5))
+
+    N: Integer (how many documents to retrieve). By default N = 3
+
+    Returns:
+    -----------
+    a ranked list of pairs (doc_id, score) in the length of N.
+    """
+
+    return sorted([(doc_id, round(score, 5)) for doc_id, score in sim_dict.items()], key=lambda x: x[1], reverse=True)[
+           :N]
+
+
+def result_doc_to_title(arr, titles_dict):
+    result = []
+    for doc_id, score in arr:
+        result.append((doc_id, score, titles_dict[doc_id]))
+    return result
+
+
+def get_page_views(pages, page_view_dict):
+    return [(doc_id, page_view_dict[doc_id]) for doc_id in pages]
+
+
+def get_page_rank(pages, data):
+    return [(page, data[page]) for page in pages if page in data]
--- search_frontend.py
+++ search_frontend.py
@@ -1,20 +1,49 @@
+import pickle
+from collections import defaultdict, Counter
+import numpy as np
 from flask import Flask, request, jsonify
+from inverted_index_gcp import InvertedIndex
+import pandas as pd
+import math
+from functions import *
+from datetime import datetime
+
 
 class MyFlaskApp(Flask):
     def run(self, host=None, port=None, debug=None, **options):
         super(MyFlaskApp, self).run(host=host, port=port, debug=debug, **options)
 
+
 app = MyFlaskApp(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 
+def init():
+    global body_index
+    body_index = InvertedIndex.read_index("./body_index", "body_index")
+    global title_index
+    title_index = InvertedIndex.read_index("./title_index", "title_index")
+    global anchor_index
+    anchor_index = InvertedIndex.read_index("./anchor_index", "anchor_index")
+    global docid_title_dict
+    with open('docid_title_dict.pkl', 'rb') as handle:
+        docid_title_dict = pickle.load(handle)
+    global pageRank_dict
+    with open('pageRank_dict.pickle', 'rb') as handle:
+        pageRank_dict = pickle.load(handle)
+    global page_view_dict
+    with open('pageviews_dict.pkl', 'rb') as handle:
+        page_view_dict = pickle.load(handle)
+
+
+
 @app.route("/search")
 def search():
-    ''' Returns up to a 100 of your best search results for the query. This is 
+    ''' Returns up to a 100 of your best search results for the query. This is
         the place to put forward your best search engine, and you are free to
-        implement the retrieval whoever you'd like within the bound of the 
+        implement the retrieval whoever you'd like within the bound of the
         project requirements (efficiency, quality, etc.). That means it is up to
-        you to decide on whether to use stemming, remove stopwords, use 
+        you to decide on whether to use stemming, remove stopwords, use
         PageRank, query expansion, etc.
 
         To issue a query navigate to a URL like:
@@ -23,50 +52,63 @@ def search():
         if you're using ngrok on Colab or your external IP on GCP.
     Returns:
     --------
-        list of up to 100 search results, ordered from best to worst where each 
+        list of up to 100 search results, ordered from best to worst where each
         element is a tuple (wiki_id, title).
     '''
     res = []
     query = request.args.get('query', '')
     if len(query) == 0:
-      return jsonify(res)
+        return jsonify(res)
     # BEGIN SOLUTION
-
+    query = query.lower()
+    query = query.split()
+    cosine_similarity_dict = cosine_similarity(query, body_index)
+    res = get_top_n(cosine_similarity_dict)
     # END SOLUTION
     return jsonify(res)
+
 
 @app.route("/search_body")
 def search_body():
     ''' Returns up to a 100 search results for the query using TFIDF AND COSINE
-        SIMILARITY OF THE BODY OF ARTICLES ONLY. DO NOT use stemming. DO USE the 
-        staff-provided tokenizer from Assignment 3 (GCP part) to do the 
-        tokenization and remove stopwords. 
+            SIMILARITY OF THE BODY OF ARTICLES ONLY. DO NOT use stemming. DO USE the
+            staff-provided tokenizer from Assignment 3 (GCP part) to do the
+            tokenization and remove stopwords.
 
-        To issue a query navigate to a URL like:
-         http://YOUR_SERVER_DOMAIN/search_body?query=hello+world
-        where YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
-        if you're using ngrok on Colab or your external IP on GCP.
-    Returns:
-    --------
-        list of up to 100 search results, ordered from best to worst where each 
-        element is a tuple (wiki_id, title).
-    '''
+            To issue a query navigate to a URL like:
+             http://YOUR_SERVER_DOMAIN/search_body?query=hello+world
+            where YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
+            if you're using ngrok on Colab or your external IP on GCP.
+        Returns:
+        --------
+            list of up to 100 search results, ordered from best to worst where each
+            element is a tuple (wiki_id, title).
+        '''
     res = []
     query = request.args.get('query', '')
     if len(query) == 0:
-      return jsonify(res)
+        return jsonify(res)
     # BEGIN SOLUTION
+    query = query.lower()
+    query = query.split()
+    cosine_similarity_dict = cosine_similarity(query, body_index)
+    res = get_top_n(cosine_similarity_dict)
 
     # END SOLUTION
     return jsonify(res)
 
+
 @app.route("/search_title")
 def search_title():
-    ''' Returns ALL (not just top 100) search results that contain A QUERY WORD 
-        IN THE TITLE of articles, ordered in descending order of the NUMBER OF 
-        QUERY WORDS that appear in the title. For example, a document with a 
-        title that matches two of the query words will be ranked before a 
-        document with a title that matches only one query term. 
+    ''' Returns ALL (not just top 100) search results that contain A QUERY WORD
+        IN THE TITLE of articles, ordered in descending order of the NUMBER OF
+        DISTINCT QUERY WORDS that appear in the title. DO NOT use stemming. DO
+        USE the staff-provided tokenizer from Assignment 3 (GCP part) to do the
+        tokenization and remove stopwords. For example, a document
+        with a title that matches two distinct query words will be ranked before a
+        document with a title that matches only one distinct query word,
+        regardless of the number of times the term appeared in the title (or
+        query).
 
         Test this by navigating to the a URL like:
          http://YOUR_SERVER_DOMAIN/search_title?query=hello+world
@@ -74,98 +116,111 @@ def search_title():
         if you're using ngrok on Colab or your external IP on GCP.
     Returns:
     --------
-        list of ALL (not just top 100) search results, ordered from best to 
+        list of ALL (not just top 100) search results, ordered from best to
         worst where each element is a tuple (wiki_id, title).
     '''
     res = []
     query = request.args.get('query', '')
     if len(query) == 0:
-      return jsonify(res)
+        return jsonify(res)
     # BEGIN SOLUTION
-
+    query = query.lower()
+    query = query.split()
+    boolean_similarity_dict = boolean_similarity(query, title_index)
+    res = get_top_n(boolean_similarity_dict,len(boolean_similarity_dict))
     # END SOLUTION
     return jsonify(res)
+
 
 @app.route("/search_anchor")
 def search_anchor():
-    ''' Returns ALL (not just top 100) search results that contain A QUERY WORD 
-        IN THE ANCHOR TEXT of articles, ordered in descending order of the 
-        NUMBER OF QUERY WORDS that appear in anchor text linking to the page. 
-        For example, a document with a anchor text that matches two of the 
-        query words will be ranked before a document with anchor text that 
-        matches only one query term. 
+    ''' Returns ALL (not just top 100) search results that contain A QUERY WORD
+            IN THE ANCHOR TEXT of articles, ordered in descending order of the
+            NUMBER OF QUERY WORDS that appear in anchor text linking to the page.
+            DO NOT use stemming. DO USE the staff-provided tokenizer from Assignment
+            3 (GCP part) to do the tokenization and remove stopwords. For example,
+            a document with a anchor text that matches two distinct query words will
+            be ranked before a document with anchor text that matches only one
+            distinct query word, regardless of the number of times the term appeared
+            in the anchor text (or query).
 
-        Test this by navigating to the a URL like:
-         http://YOUR_SERVER_DOMAIN/search_anchor?query=hello+world
-        where YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
-        if you're using ngrok on Colab or your external IP on GCP.
-    Returns:
-    --------
-        list of ALL (not just top 100) search results, ordered from best to 
-        worst where each element is a tuple (wiki_id, title).
-    '''
+            Test this by navigating to the a URL like:
+             http://YOUR_SERVER_DOMAIN/search_anchor?query=hello+world
+            where YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
+            if you're using ngrok on Colab or your external IP on GCP.
+        Returns:
+        --------
+            list of ALL (not just top 100) search results, ordered from best to
+            worst where each element is a tuple (wiki_id, title).
+        '''
     res = []
     query = request.args.get('query', '')
     if len(query) == 0:
-      return jsonify(res)
+        return jsonify(res)
     # BEGIN SOLUTION
-    
+    query = query.lower()
+    query = query.split()
+    boolean_similarity_dict = boolean_similarity(query, anchor_index)
+    res = get_top_n(boolean_similarity_dict, len(boolean_similarity_dict))
     # END SOLUTION
     return jsonify(res)
+
 
 @app.route("/get_pagerank", methods=['POST'])
 def get_pagerank():
-    ''' Returns PageRank values for a list of provided wiki article IDs. 
+    ''' Returns PageRank values for a list of provided wiki article IDs.
 
-        Test this by issuing a POST request to a URL like:
-          http://YOUR_SERVER_DOMAIN/get_pagerank
-        with a json payload of the list of article ids. In python do:
-          import requests
-          requests.post('http://YOUR_SERVER_DOMAIN/get_pagerank', json=[1,5,8])
-        As before YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
-        if you're using ngrok on Colab or your external IP on GCP.
-    Returns:
-    --------
-        list of floats:
-          list of PageRank scores that correrspond to the provided article IDs.
-    '''
+            Test this by issuing a POST request to a URL like:
+              http://YOUR_SERVER_DOMAIN/get_pagerank
+            with a json payload of the list of article ids. In python do:
+              import requests
+              requests.post('http://YOUR_SERVER_DOMAIN/get_pagerank', json=[1,5,8])
+            As before YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
+            if you're using ngrok on Colab or your external IP on GCP.
+        Returns:
+        --------
+            list of floats:
+              list of PageRank scores that correrspond to the provided article IDs.
+        '''
     res = []
     wiki_ids = request.get_json()
     if len(wiki_ids) == 0:
-      return jsonify(res)
+        return jsonify(res)
     # BEGIN SOLUTION
-
+    res = get_page_rank(wiki_ids, pageRank_dict)
     # END SOLUTION
     return jsonify(res)
+
 
 @app.route("/get_pageview", methods=['POST'])
 def get_pageview():
     ''' Returns the number of page views that each of the provide wiki articles
-        had in August 2021.
+            had in August 2021.
 
-        Test this by issuing a POST request to a URL like:
-          http://YOUR_SERVER_DOMAIN/get_pageview
-        with a json payload of the list of article ids. In python do:
-          import requests
-          requests.post('http://YOUR_SERVER_DOMAIN/get_pageview', json=[1,5,8])
-        As before YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
-        if you're using ngrok on Colab or your external IP on GCP.
-    Returns:
-    --------
-        list of ints:
-          list of page view numbers from August 2021 that correrspond to the 
-          provided list article IDs.
-    '''
+            Test this by issuing a POST request to a URL like:
+              http://YOUR_SERVER_DOMAIN/get_pageview
+            with a json payload of the list of article ids. In python do:
+              import requests
+              requests.post('http://YOUR_SERVER_DOMAIN/get_pageview', json=[1,5,8])
+            As before YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
+            if you're using ngrok on Colab or your external IP on GCP.
+        Returns:
+        --------
+            list of ints:
+              list of page view numbers from August 2021 that correrspond to the
+              provided list article IDs.
+        '''
     res = []
     wiki_ids = request.get_json()
     if len(wiki_ids) == 0:
-      return jsonify(res)
+        return jsonify(res)
     # BEGIN SOLUTION
-
+    res = get_page_views(wiki_ids,page_view_dict)
     # END SOLUTION
     return jsonify(res)
 
 
 if __name__ == '__main__':
     # run the Flask RESTful API, make the server publicly available (host='0.0.0.0') on port 8080
+    init()
     app.run(host='0.0.0.0', port=8080, debug=True)
